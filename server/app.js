50f112868d409177bcf5a46e348a1f927632fdd0
RobinXu <rxumail@gmail.com>
2018-11-08 14:47:26 +0800

    server ready

---
+++ server/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+var app = express();
+
+var port = process.env.PORT || 3005 // sets a relative port
+
+const CLIENT_FOLDER = path.join(__dirname + '/../client');
+
+app.use(express.static(CLIENT_FOLDER)); 
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true}));
+app.use(bodyParser.json({limit: "50mb"}));
+
+
+app.listen(port, function() {
+    console.log('We have successfully connected to port: ', port);
+  });
+
